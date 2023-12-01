615dda642caf3e9e2972b8624bb840bbc0aa5ecb
Kema14 <kelvin_ma23@hotmail.com>
2023-11-30 22:12:15 -0800

    done part 2

--- 01_solution.py
+++ 01_solution.py
@@ -1,8 +1,28 @@
+import re
+
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+
+def split_by_numbers(input):
+    pattern = r'(0|1|2|3|4|5|6|7|8|9|zero|one|two|three|four|five|six|seven|eight|nine)'
+    # Splitting the string using the regex pattern
+    return re.findall(pattern, input)
+
+num_words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+def map_to_num(list_of_nums):
+    # map from word to a value
+    # print(list_of_nums)
+    return map(lambda x: num_words.index(x) + 1 if x in num_words else int(x), list_of_nums)
+
+def sum_first_and_last(input):
+    input = list(input)
+    return int(str(input[0]) + str(input[-1]))
+
 def extract_calibration_value(file_name):
     with open(file_name) as f:
         # On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
-        numbers = map(lambda x: ''.join(filter(str.isdigit, x)), f)
-        sums = map(lambda x: int(x[0] + x[-1]), numbers)
+        numbers = map(split_by_numbers, f)
+        all_numbers = map(map_to_num, numbers)
+        sums = map(sum_first_and_last, all_numbers)
         total = sum(sums)
         return total
 
